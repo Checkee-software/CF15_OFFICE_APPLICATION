2dfbdde0bdd017832208ea2d22a46c0764202ee4
Checkee-software <checkeesoftware@gmail.com>
2025-06-27 20:00:17 +0700

    comment onesign id

--- index.js
+++ index.js
@@ -5,10 +5,10 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import { OneSignal } from 'react-native-onesignal';
+// import { OneSignal } from 'react-native-onesignal';
 
-OneSignal.initialize('64fd0b66-e4fe-431f-b95f-1ef857e1adfd');
-//YÊU CẦU QUYỀN gửi thông báo từ người dùng
-OneSignal.Notifications.requestPermission(true);
+// OneSignal.initialize('64fd0b66-e4fe-431f-b95f-1ef857e1adfd');
+// //YÊU CẦU QUYỀN gửi thông báo từ người dùng
+// OneSignal.Notifications.requestPermission(true);
 
 AppRegistry.registerComponent(appName, () => App);
