23dc8e8b4a38ad90ec6786b4dffbb6559afe8bac
HieuLe2723 <hieunguyenle02072003@gmail.com>
2026-06-01 09:10:09 +0700

    Update ENV configuration

--- src/config/ENV.ts
+++ src/config/ENV.ts
@@ -1,5 +1,6 @@
 namespace ENV {
-    export const BACKEND_URL: string = 'https://mdj9mnt2-3015.asse.devtunnels.ms';
+    export const BACKEND_URL: string = 'http://cf15officeservice.checkee.vn';
+    export const MYSIGN_ENABLED: boolean = false;
     // 'http://cf15dev.checkee.vn'
     //'https://zlhct5tw-3015.asse.devtunnels.ms'; //url pc Phuc
     //'https://9g7ltw0j-3015.asse.devtunnels.ms'; //url laptop Phuc
